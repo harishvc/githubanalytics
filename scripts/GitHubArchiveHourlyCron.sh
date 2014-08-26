f14692e0cac6503474705239013f2a74ff58b182
Harish Chakravarthy <harishvc@gmail.com>
2014-08-25 22:13:54 -0700

    Scripts to run via cron/scheduler

---
+++ scripts/GitHubArchiveHourlyCron.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /home/harishvc/github/githubanalytics
+usr/local/bin/node FetchParseGitHubArchive.js  >> /home/harishvc/github/githubanalytics/output/FetchParseGitHubArchive.log
---
+++ scripts/HourlyCron.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /home/harishvc/github/githubanalytics
+python cronTrainman.py  >> /home/harishvc/github/githubanalytics/output/cronoutput.txt
