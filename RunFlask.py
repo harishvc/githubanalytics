1893f9cad075b5d3c687ecf9b81e54bb15613545
Harish Chakravarthy <harishvc@gmail.com>
2014-08-27 21:26:02 -0700

    change host name

--- RunFlask.py
+++ RunFlask.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
 from app import app
 #app.run(debug = True)
-#app.run(host="127.0.0.1",port="5000")
-app.run(host="23.239.20.13",port="5000")
+app.run(host="127.0.0.1",port="5000")
+#app.run(host="23.239.20.13",port="5000")
