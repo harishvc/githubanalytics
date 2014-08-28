2610677ea798a25ebe099b4f2bf9f5d0e57f8b7f
harishvc <harishvc@gmail.com>
2014-08-27 23:53:46 -0700

    print host and port info when starting up

--- RunFlask.py
+++ RunFlask.py
@@ -10,4 +10,5 @@ import os
 from app import app  
 port = int(os.environ.get('PORT', 5000)) 
 app.run(debug = True)
+print "starting flask server hostname:% port:%" % (host, port)
 app.run(host='0.0.0.0', port=port)
