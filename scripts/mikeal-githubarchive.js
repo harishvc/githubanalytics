f1b5f491e5919f8e245db415deebd93211196bfe
harishvc <harishvc@gmail.com>
2014-08-29 22:42:25 -0700

    Add header to resolve zlib problem

--- scripts/mikeal-githubarchive.js
+++ scripts/mikeal-githubarchive.js
@@ -10,6 +10,18 @@ var util = require('util')
   , request = require('request').defaults({pool:{maxSockets:1}})
   ;
 
+var headers = {
+  "accept-charset" : "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+  "accept-language" : "en-US,en;q=0.8",
+  "accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+  "user-agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2",
+  "accept-encoding" : "gzip,deflate",
+}
+ 
+var options = {
+  headers: headers
+}
+
 function CallbackStream (cb) {
   this.readable = true
   this.writable = true
