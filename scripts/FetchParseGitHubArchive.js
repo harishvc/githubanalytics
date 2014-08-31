5d227c0466869a444c7b7f412769993d3d0f4066
harishvc <harishvc@gmail.com>
2014-08-30 22:34:40 -0700

    Add commit messages

--- scripts/FetchParseGitHubArchive.js
+++ scripts/FetchParseGitHubArchive.js
@@ -1,11 +1,16 @@
+console.log ("0: I am here");
 var archive = require('./mikeal-githubarchive.js');
+console.log ("1: I am here");
 var path = require('path');
 var assert = require('assert');
 var underscore = require('underscore');
 var moment = require('moment');
 var fs = require('fs');
+console.log ("2: I am here");
 
 TimeAgo = moment().subtract(2, 'hours').format("YYYY-MM-DD-HH");
+console.log ("3: I am here");
+
 //Testing
 //TimeAgo ="2014-08-28-10"  
 URL = "http://data.githubarchive.org/" + TimeAgo + ".json.gz";
