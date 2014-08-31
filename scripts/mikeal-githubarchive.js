8ae7e6a77a674d80379963ac835a8ddc36c1b036
harishvc <harishvc@gmail.com>
2014-08-30 21:06:20 -0700

    Modify logic to handle requests based on Nick Fishman post

--- scripts/mikeal-githubarchive.js
+++ scripts/mikeal-githubarchive.js
@@ -138,10 +138,24 @@ function wrap (obj) {
 }
 
 module.exports.request = function (url) {
-  return wrap(request(url).pipe(zlib.createGunzip()).pipe(jsonstream.parse()))
+  // http://nickfishman.com/post/49533681471/nodejs-http-requests-with-gzip-deflate-compression
+  var headers = {
+  "accept-charset" : "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+  "accept-language" : "en-US,en;q=0.8",
+  "accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+  "user-agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2",
+  "accept-encoding" : "gzip,deflate",
+  } 
+  var options = {
+  url: url,
+  headers: headers
+  }
+  return wrap(request(options).pipe(zlib.createGunzip()).pipe(jsonstream.parse()))
 }
 
 module.exports.readFile = function (path, options) {
+
+
   var f = fs.createReadStream(path)
     , z = new zlib.createGunzip() 
     , j = jsonstream.parse()
