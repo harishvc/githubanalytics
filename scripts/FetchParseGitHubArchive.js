1a3c50068785a725b36c59ab4cbf7cece2b54bba
harishvc <harishvc@gmail.com>
2014-08-30 23:47:35 -0700

    modify node.js script to be run from Heroku scheduler - changes to path and env

--- scripts/FetchParseGitHubArchive.js
+++ scripts/FetchParseGitHubArchive.js
@@ -1,26 +1,31 @@
-console.log ("0: I am here");
+#!/usr/bin/env node
+//http://www.spacjer.com/blog/2014/02/10/defining-node-dot-js-task-for-heroku-scheduler/
+
+var fs = require('fs');
+var util = require('util');
+var logStream = fs.createWriteStream('../output/FetchParseGitHubArchive.log', {flags: 'a'});
+console.log = function(d) { 
+  logStream.write(util.format(d) + '\n');
+};
+
 var archive = require('./mikeal-githubarchive.js');
-console.log ("1: I am here");
 var path = require('path');
 var assert = require('assert');
 var underscore = require('underscore');
 var moment = require('moment');
-var fs = require('fs');
-console.log ("2: I am here");
-
-TimeAgo = moment().subtract(2, 'hours').format("YYYY-MM-DD-HH");
-console.log ("3: I am here");
+var TimeNow = moment().format("YYYY-MM-DD HH:MM:SS");
+var TimeAgo = moment().subtract(2, 'hours').format("YYYY-MM-DD-HH");
 
 //Testing
 //TimeAgo ="2014-08-28-10"  
 URL = "http://data.githubarchive.org/" + TimeAgo + ".json.gz";
-console.log ("Processing: " + URL);
+console.log (TimeNow + " processing " + URL);
 
 //Fetch GitHub Archives & Generate JSON file with 'PushEvent' event notifications
 var a = archive(URL, {gzip:true});
 var com = a.MyParser(function (err, commits) {
   //console.log(JSON.stringify(com.commits));
-  fs.writeFile("./data/PushEvent.json", JSON.stringify(com.commits), function (err) {
+  fs.writeFile("../data/PushEvent.json", JSON.stringify(com.commits), function (err) {
 	  if (err) return console.log(err);
 	});
   if (err) return console.log(err);
