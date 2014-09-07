46dac553f61ed0459356b27139b2b20223a7efd4
harishvc <harishvc@gmail.com>
2014-09-07 00:38:31 -0700

    delete files

--- bin/GitHubArchiveHourlyCron.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -x
-cd;
-node ./scripts/FetchParseGitHubArchive.js  >> ./output/FetchParseGitHubArchive.log
--- bin/HourlyCron.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /home/harishvc/github/githubanalytics
-python cronTrainman.py  >> /home/harishvc/github/githubanalytics/output/cronoutput.txt
