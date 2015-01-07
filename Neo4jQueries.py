ced65da11762944f387b4a3fff6580fc8e8eaf4d
harishvc <harishvc@gmail.com>
2015-01-06 23:15:27 -0800

    change WHERE to URL (not name)

--- Neo4jQueries.py
+++ Neo4jQueries.py
@@ -12,7 +12,7 @@ def FindSimilarRepositories(Inputrepo):
 	output = []
 	outputString =""
 	query1= """MATCH (me)-[r1:IS_LANGUAGE|IS_OWNER|IN_ORGANIZATION|IS_ACTOR]->(stuff)<-[r2:IS_LANGUAGE|IS_OWNER|IN_ORGANIZATION|IS_ACTOR]-(repo) """
-	query2="WHERE me.name = " + "\"" + Inputrepo + "\"" 
+	query2="WHERE me.url = " + "\"" + Inputrepo + "\"" 
 	query3=""" AND type (r1) = type (r2)
         	   RETURN repo.name as reponame, repo.url as url, count(stuff) as count                
            	   ORDER BY count(stuff) DESC LIMIT 5"""               
