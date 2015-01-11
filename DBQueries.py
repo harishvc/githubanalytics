b1d4baf230133a1a585c27024634f73085466243
harishvc <harishvc@gmail.com>
2015-01-11 09:13:57 -0800

    move functions around;update  in RepoQuery() & Search() to include 'sha'

--- DBQueries.py
+++ DBQueries.py
@@ -37,6 +37,8 @@ LimitActiveUsers=5
 def numformat(value):
     return "{:,}".format(value)
 
+
+
 def TotalEntries (type):
     return ("<div class=\"digital\">" + numformat(db.count()) + "</div> " + type)
 
@@ -49,59 +51,7 @@ def FindDistinct(fieldName,type):
     mycursor = db.aggregate(pipeline)
     for row in mycursor["result"]:
         return ("<div class=\"digital\">" + numformat(row['count']) + "</div> " + type)
-    
-def ProcessRepositories(repoName):
-    global ShowSuggestion
-    mycursor = RepoQuery(repoName)
-    if (len(mycursor["result"]) == 0):
-        return ("EMPTY")
-    else:       
-        myreturn =""
-        #Add recommendation using neo4j
-        #similarRepos = Neo4jQueries.FindSimilarRepositories(repoName)
-        similarRepos = FindSimilarRepositories(repoName)
-        for record in mycursor["result"]:
-            myreturn = "<a href=" + str(record['url']) + ">" + str(record['name']) + "</a>"
-            myreturn += "&nbsp;Language: " + str(record['language']) + "&nbsp;#commits: " + str(record['count'])
-            myreturn += "</br>" + record['description'].encode('utf-8').strip()
-            myreturn += similarRepos
-            myreturn += "</br><b>Commits from</b>: " +  ', '.join(record['actorname']).encode('utf-8').strip()
-            myreturn += "</br><b>Comments</b>:<ul>" 
-            for x in record["comment"]: 
-                #convert milliseconds to seconds
-                #pop first element in the array
-                sha = record['sha'].pop(-1).encode('utf-8').strip()
-                myreturn += "<li>" +  "<a href=" + str(record['url']) + "/commit/" + sha + ">" + MyMoment.HTM(record["created_at"].pop(0)/1000) + "</a>" \
-                            + "&nbsp;&nbsp;" + x.encode('utf-8').strip() + "</li>" 
-            myreturn +="</ul>"
-            #app.logger.debug (myreturn)
-            
-        #Add recommendation using neo4j
-        #myreturn += Neo4jQueries.FindSimilarRepositories(repoName)
-                    
-        return(myreturn)
-   
-def ProcessQuery(query):
-    global ShowSuggestion
-    ShowSuggestion = False
-    if (query == ""):
-        return ""
-    else: 
-        app.logger.debug("processing ............ ->%s<-" ,  query)
-        if (query == "active repositories"):
-             return FindDistinct ('$url',"repositories")
-        elif  (query == "active users"):
-            return FindDistinct ('$actorlogin', "users")
-        elif  (query == "active languages"):   
-            return FindDistinct ('$language', "languages") 
-        elif  (query == "total commits"):   
-            return TotalEntries("commits")
-        elif  (query.startswith("repository")):
-            return ProcessRepositories(query.replace('repository ', ''))
-        else:
-            #return ("EMPTY")
-            return Search(query) 
-    
+
 def FindOneTimeStamp(type):
     pipeline= [
            { '$match': {} }, 
@@ -142,34 +92,6 @@ def ActiveLanguagesBubble ():
            a1.append({"name":str(record["language"]), "size":str(record["count"]),"size2":str(t1)});  
     #create custom dictionary
     return ({"name": "something", "children": a1})
-
-def RepoQuery (repoURL):
-    pipeline= [
-           { '$match': {"url": repoURL}}, 
-           { '$group': {'_id': {'url': '$url',  'name': "$name", 'language': "$language",'description': "$description"}, '_a1': {"$addToSet": "$actorname"} ,'_a2': {"$push": "$comment"},'_a3': {"$push": "$created_at"},'_a4': {"$addToSet": "$sha"},'count': { '$sum' : 1 }}},
-           { '$project': { '_id': 0, 'url': '$_id.url', 'count': '$count',  'name': "$_id.name", 'language': "$_id.language",'description': "$_id.description", 'actorname': "$_a1",'comment': "$_a2",'created_at': "$_a3", 'sha': "$_a4" } },
-           ]
-    mycursor = db.aggregate(pipeline)
-    return mycursor
-
-#Get similar repositories
-def FindSimilarRepositories(repoURL):
-    output = ""
-    pipeline= [
-           { '$match': {"url": repoURL, "type" : "similarrepositories"}}, 
-           { '$group': {'_id': {'similar': '$similar'}}},
-           { '$project': { '_id': 0, 'similar': '$_id.similar'}},
-           ]
-    mycursor = db.aggregate(pipeline)
-    for record in mycursor["result"]:
-        output = str(record['similar'])
-        
-    if len(output) !=0 :
-        return ("<br/><b>Similar Repositories</b>: " + output)
-    else:
-        return output
-
-    
     
 def ActiveRepositories ():
     pipeline= [
@@ -184,6 +106,7 @@ def ActiveRepositories ():
     return mycursor
 
 #Source:http://stackoverflow.com/questions/25861471/mongo-aggregation-distinct               
+
 def ActiveRepositoriesGroupedByDistinctUsers ():
     users =[]
     commits =[]
@@ -202,9 +125,8 @@ def ActiveRepositoriesGroupedByDistinctUsers ():
         commits.append({"month": str(record["repo_url"]), "reponame": str(record["repo_name"]),"count": str(record["total_commits"])})                    
     
     t2 = [{"data":users,"name": "# Unique users"},{"data":commits,"name": "# Commits"} ]
-    return mycursor, t2
-    
-    
+    return mycursor, t2        
+
 def ActiveUsers ():
     pipeline= [
            { '$match': {}}, 
@@ -217,6 +139,7 @@ def ActiveUsers ():
     return mycursor
 
 #Source:http://stackoverflow.com/questions/25840723/mongo-aggregation-grouped-by-sum
+
 def CommitFrequency ():
     output =[]
     range0 = range1 = range2 = range3 = range4 = range5 = range6 = 0
@@ -251,7 +174,99 @@ def CommitFrequency ():
     output.append({"commits": ">20", "count": range6,"count2": numformat(range6)})
     return output
 
-#Search in fields name, language & description
+###################################################
+####################################################    
+
+
+
+def ProcessQuery(query):
+    global ShowSuggestion
+    ShowSuggestion = False
+    if (query == ""):
+        return ""
+    else: 
+        app.logger.debug("processing ............ ->%s<-" ,  query)
+        if (query == "active repositories"):
+             return FindDistinct ('$url',"repositories")
+        elif  (query == "active users"):
+            return FindDistinct ('$actorlogin', "users")
+        elif  (query == "active languages"):   
+            return FindDistinct ('$language', "languages") 
+        elif  (query == "total commits"):   
+            return TotalEntries("commits")
+        elif  (query.startswith("repository")):
+            return ProcessRepositories(query.replace('repository ', ''))
+        else:
+            #return ("EMPTY")
+            #Global Search
+            return Search(query) 
+        
+        
+def ProcessRepositories(repoName):
+    global ShowSuggestion
+    mycursor = RepoQuery(repoName)
+    if (len(mycursor["result"]) == 0):
+        return ("EMPTY")
+    else:       
+        myreturn =""
+        #Add recommendation using neo4j
+        #similarRepos = Neo4jQueries.FindSimilarRepositories(repoName)
+        similarRepos = FindSimilarRepositories(repoName)
+        for record in mycursor["result"]:
+            myreturn = "<a href=" + str(record['url']) + ">" + str(record['name']) + "</a>"
+            myreturn += "&nbsp;Language: " + str(record['language']) + "&nbsp;#commits: " + str(record['count'])
+            #Handle None & empty description
+            if ('description' in record):
+                if ( (record['description'] != None) and (len(record['description'])) > 0):
+                    myreturn += "</br>" + record['description'].encode('utf-8').strip()
+                    #print "Description is not empty -->" , record['description']
+            myreturn += similarRepos
+            myreturn += "</br><b>Commits from</b>: " +  ', '.join(record['actorname']).encode('utf-8').strip()
+            myreturn += "</br><b>Comments</b>:<ul>" 
+            for x in record["comment"]: 
+                #convert milliseconds to seconds
+                #pop first element in the array
+                sha = record['sha'].pop(-1).encode('utf-8').strip()
+                myreturn += "<li>" +  "<a href=" + str(record['url']) + "/commit/" + sha + ">" + MyMoment.HTM(record["created_at"].pop(0)/1000) + "</a>" \
+                            + "&nbsp;&nbsp;" + x.encode('utf-8').strip() + "</li>" 
+            myreturn +="</ul>"
+            #app.logger.debug (myreturn)
+                    
+        return(myreturn)
+
+#Find information about a repository   
+def RepoQuery (repoURL):
+    pipeline= [
+           { '$match' : { 'url' : repoURL , 'sha': { '$exists': True }}  },
+           { '$group': {'_id': {'url': '$url',  'name': "$name", 'language': "$language",'description': "$description"}, '_a1': {"$addToSet": "$actorname"} ,'_a2': {"$push": "$comment"},'_a3': {"$push": "$created_at"},'_a4': {"$addToSet": "$sha"},'count': { '$sum' : 1 }}},
+           { '$project': { '_id': 0, 'url': '$_id.url', 'count': '$count',  'name': "$_id.name", 'language': "$_id.language",'description': "$_id.description", 'actorname': "$_a1",'comment': "$_a2",'created_at': "$_a3", 'sha': "$_a4" } },
+           ]
+    mycursor = db.aggregate(pipeline)
+    #Debug
+    #for row in mycursor["result"]:
+    #    print row
+        
+    return mycursor
+
+#Find similar repositories
+def FindSimilarRepositories(repoURL):
+    output = ""
+    pipeline= [
+           { '$match': {"url": repoURL, "type" : "similarrepositories"}}, 
+           { '$group': {'_id': {'similar': '$similar'}}},
+           { '$project': { '_id': 0, 'similar': '$_id.similar'}},
+           ]
+    mycursor = db.aggregate(pipeline)
+    for record in mycursor["result"]:
+        output = str(record['similar'])
+        
+    if len(output) !=0 :
+        return ("<br/><b>Similar Repositories</b>: " + output)
+    else:
+        return output
+
+    
+#Global search in property fields name, language & description
 def Search(query):
     path1 = "<a href=\"/?q=repository "
     path2 = "&amp;action=Search\">"
@@ -259,7 +274,7 @@ def Search(query):
     output = ""
     qregx = re.compile(query, re.IGNORECASE)
     pipeline = [
-           { '$match': {'$or' : [{'name':qregx},{'description':qregx},{ 'language': qregx }] }},
+           { '$match': {'$or' : [{'name':qregx},{'description':qregx},{ 'language': qregx }] , 'sha': { '$exists': True } }},
            { '$group':  {'_id': {'url': '$url',  'name': "$name", 'language': "$language",'description': "$description"}}},
            { '$project': { '_id': 0, 'url': '$_id.url', 'name': "$_id.name", 'language': "$_id.language",'description': "$_id.description"}},
            { '$sort' : { 'name': 1 }},
