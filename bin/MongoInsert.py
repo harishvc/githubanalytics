0b615d5596b9573f48c6981c567dbe25c60adc97
harishvc <harishvc@gmail.com>
2015-01-18 11:33:04 -0800

    Major changes to automate finding similar repositories using Neo4j and then inserting results inside mongodb

--- bin/MongoInsert.py
+++ bin/MongoInsert.py
@@ -1,16 +1,34 @@
 from py2neo import Graph
-import os.path
+import os.path,  sys
 from flask import Flask
 app = Flask(__name__)
 from pymongo import MongoClient
+import json
+import re
+import cgi
 
 
-#TODO: delete document before entry
+#Local Module
+sys.path.append('../')
+import MyMoment
 
+#Global variables
+TSR = {}
+TSR["type"] =  "similarrepositories"
+
+#DB connections
 graph = Graph(os.environ['neoURL'])
 MONGO_URL = os.environ['connectURL']
 connection = MongoClient(MONGO_URL)
-db = connection.githublive.pusheventCapped
+db = connection.githublive.pushevent
+		
+#Create queries for MongoDB insert
+MONGODBFILE="mongodb-" + MyMoment.FT() + ".txt"
+#MONGODBFILE="mongodb-17Jan2015-160635.txt"
+
+#Write to external file for batch process
+f = open(MONGODBFILE,"w")
+#f = open(MONGODBFILE,"r")
 
 def SimilarRepositories(Inputrepo):
 	output = []
@@ -29,23 +47,54 @@ def SimilarRepositories(Inputrepo):
 	for entries in result: 
 		output.append(path1 + entries.url.encode('utf-8').strip() + path2 + entries.reponame.encode('utf-8').strip() + path3)                        
 
+			 
  	if len(output) !=0 : 
  		outputString +=  ", ".join(output)
- 		#Delete old entry
- 		#db.remove({'type':'similarrepositories','url': Inputrepo})
- 		
- 		#Insert to MongoDB
- 		mongo_record_insert = {'type': 'similarrepositories', 'url': Inputrepo, 'similar': outputString}
- 		db.insert(mongo_record_insert)
- 
+ 		#Escape 		
+		tmp1 = outputString.replace('"','\\"')
+		tmp2 = cgi.escape(tmp1)
+		f.write('\'type\'' + ':' + '\'similarrepositories\'' + ',' + '\'url\'' + ':' +  '\'' + Inputrepo + '\',' + '\'similar\'' + ':\'' +  tmp2 + '\'' + "\n")          
+ 	
 
 def FindAllRepositories():
+	print MyMoment.MT(), "start: generating similar repositories ..." , MONGODBFILE
 	query = "MATCH (n:`Repository`) RETURN n.url as url"
 	result = graph.cypher.execute(query)
 	for entries in result: 
-		print "Processing ..... ", entries.url
+		#print "Processing ..... ", entries.url
 		SimilarRepositories(entries.url)
+  	print MyMoment.MT(), "end: generating similar repositories ..." , MONGODBFILE  
+  	f.close()
 		
 
+def Insert2Mongo():
+	if (os.path.exists(MONGODBFILE)):		
+		#Delete old entries
+		print MyMoment.MT(), "start: deleting old similar repositories inside mongodb ..."
+		db.remove(TSR,safe=True)
+		print MyMoment.MT(), "end: deleting old similar repositories inside mongodb ..."
+		#Process line by line
+		print MyMoment.MT(), "start: inserting similar repositories inside mongodb ..."
+		#Bulk insert
+		bulk = db.initialize_unordered_bulk_op()
+		d = {}
+		ctr = 0
+		with open(MONGODBFILE) as f:
+			for line in f.readlines():
+				ctr = ctr + 1 
+				s = "{" + line + "}"
+				json_acceptable_string = s.replace("'", "\"")
+				d = json.loads(json_acceptable_string)
+				bulk.insert(d)
+		#Batch execute        
+		bulk.execute()
+		print MyMoment.MT(), "end: inserting similar repositories inside mongodb ... inserted " , ctr
+		#Archive queries
+		destination = "./ARCHIVE/" + MONGODBFILE
+		os.rename(MONGODBFILE, destination)
+	else:
+		print MyMoment.MT(), "error:", MONGODBFILE, " does not exist"
+
+	
 FindAllRepositories()
-		
+Insert2Mongo()		
