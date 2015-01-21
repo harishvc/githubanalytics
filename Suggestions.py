a69b3cb173a5dc7fd0b8bd4862d007bdc4f8eb97
harishvc <harishvc@gmail.com>
2015-01-20 21:11:19 -0800

    remove active languages

--- Suggestions.py
+++ Suggestions.py
@@ -4,6 +4,5 @@ def RandomQuerySuggestions():
    foo =    ["<a href=\'/?q=active+repositories&action=Ask+GitHub\'>active repositories</a>",
             "<a href=\'/?q=active+users&action=Ask+GitHub\'>active users</a>",
             "<a href=\'/?q=total+commits&action=Ask+GitHub\'>total commits</a>",
-            "<a href=\'/?q=active+languages&action=Ask+GitHub\'>active languages</a>"
             ]
    return("Suggestion: " + random.choice(foo))
