c4e677d57f6f6f3591b0563779e06e61c2e41ae7
harishvc <harishvc@gmail.com>
2015-01-25 22:24:14 -0800

    add microseconds and context for return

--- MyMoment.py
+++ MyMoment.py
@@ -8,19 +8,23 @@ import pytz
 #1/6/2015, 8:19:34 AM PST  -> 23 hours ago
 #print HTM(1420561174000/1000)
 
-def HTM(aa):
+def HTM(aa, context):
+    #print "Processing ....", aa
     a = int(aa)
+    #a = aa
     b = int(datetime.datetime.now().strftime("%s"))
+    #print "Time NOW ...", b
     c = b - a
+    #print "Time elapsed ...", c
     days = c // 86400
     hours = c // 3600 % 24
     minutes = c // 60 % 60
     seconds = c % 60
-    ago = "ago"
-    if (days > 0): return ( str(days) + " days " + ago)
-    elif (hours > 0): return (str(hours) + " hours " + ago)
-    elif (minutes > 0): return ( str(minutes) + " minutes " + ago)
-    elif (seconds > 0): return (str(seconds) + " seconds " + ago)
+    if (days > 0): return ( str(days) + " days " + context)
+    elif (hours > 0): return (str(hours) + " hours " + context)
+    elif (minutes > 0): return ( str(minutes) + " minutes " + context)
+    elif (seconds > 0): return (str(seconds) + " seconds " + context)
+    elif (seconds == 0): return ("< 1 second")
     else: return (a)  #Error
 
 
