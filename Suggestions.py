b1512135e23d486de49d677e0585177aa8608eb6
harishvc <harishvc@gmail.com>
2015-02-01 05:32:11 -0800

    change html action label

--- Suggestions.py
+++ Suggestions.py
@@ -1,13 +1,13 @@
 import random
 
 def RandomQuerySuggestions():
-   foo =    ["<a href=\'/?q=active+repositories&action=Ask+GitHub\'>active repositories</a>",
-            "<a href=\'/?q=active+users&action=Ask+GitHub\'>active users</a>",
-            "<a href=\'/?q=total+commits&action=Ask+GitHub\'>total commits</a>",
-            "<a href=\'/?q=trending+now&action=Ask+GitHub\'>trending now</a>",
-            "<a href=\'/?q=top+active+repositories+by+contributors&action=Ask+GitHub\'>top active repositories by contributors</a>",
-            "<a href=\'/?q=top+active+repositories+by+commits&action=Ask+GitHub\'>top active repositories by commits</a>",
-            "<a href=\'/?q=top+active+repositories+by+branches&action=Ask+GitHub\'>top active repositories by branches</a>"
+   foo =    ["<a href=\'/?q=active+repositories&action=Search\'>active repositories</a>",
+            "<a href=\'/?q=active+users&action=Search\'>active users</a>",
+            "<a href=\'/?q=total+commits&action=Search\'>total commits</a>",
+            "<a href=\'/?q=trending+now&action=Search\'>trending now</a>",
+            "<a href=\'/?q=top+active+repositories+by+contributors&action=Search\'>top active repositories by contributors</a>",
+            "<a href=\'/?q=top+active+repositories+by+commits&action=Search\'>top active repositories by commits</a>",
+            "<a href=\'/?q=top+active+repositories+by+branches&action=Search\'>top active repositories by branches</a>"
             ]
    return("Suggestion: " + random.choice(foo))
 
