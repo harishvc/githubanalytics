c93f676ebf82b623e1ff44d422976fbff12335f3
harishvc <harishvc@gmail.com>
2015-03-08 08:31:28 -0700

    major rewrite using LOAD CVS - harishvc/githubanalytics#45

--- bin/Neo4jSync.py
+++ bin/Neo4jSync.py
@@ -13,11 +13,11 @@ import MyMoment
 
 #MongoDB & Neo4j connections
 MONGO_URL = os.environ['connectURLRead']
-connection = MongoClient(MONGO_URL)
+connection = MongoClient(MONGO_URL) 
 db = connection.githublive.pushevent
 graph = Graph(os.environ['neoURLProduction'])
-
-
+CSVPUBLICDIR = os.environ['CSVPUBLICDIR']
+CSVPUBLICURL = os.environ['CSVPUBLICURL']
 
 def numformat(value):
     return "{:,}".format(value)
@@ -40,8 +40,7 @@ def SE(s):
 			else:
 				return 0
  		else:
- 			return 0
-
+ 			return 
 #Determine Nodes and breakdown 	
 def Nodes(): 
     for record in graph.cypher.execute("match n return count(n) as count"):
@@ -66,79 +65,106 @@ def Edges():
     output = "Relations:" + str(total) + " (" + "Repositories->People:" + str(RA) + ", Repositories->Organization:" + str(RO) + ")"        
     return output
     
-	 		
-def CNR():			
-    #Find entries in the past  2 hours 15 minutes (GitHub Events are processed hourly at 15 minutes past the hour)
-    since = MyMoment.TTEM(135)
-    print MyMoment.MT() + " start: creating new nodes and relations since ", since
-    #Force Python's print function
-    sys.stdout.flush()
-    print Nodes(),Edges()
-    sys.stdout.flush()
+def CC():	
+    graph.cypher.execute("CREATE CONSTRAINT ON (a:Repository) ASSERT a.id IS UNIQUE")
+    graph.cypher.execute("CREATE CONSTRAINT ON (b:People) ASSERT b.id IS UNIQUE")
+    graph.cypher.execute("CREATE CONSTRAINT ON (c:Organization) ASSERT c.id IS UNIQUE")
+ 		
+def GenerateCSV():			
+    #Find entries in the past  1 hour 15 minutes (GitHub Events are processed hourly at 15 minutes past the hour)
+    since = MyMoment.TTEM(75)
+    print MyMoment.MT() + " start: mongodb documents since ", since
     pipeline=[
               {'$match': {'$and': [ {'sha': { '$exists': True }},{'created_at': { '$gt': since }}, {"full_name" : {'$nin': [re.compile('.*github.io.*'),re.compile('.*github.com.*')]} }]}},
               { '$group': {'_id': {'full_name': '$full_name','organization': '$organization' }, '_a1': {"$addToSet": "$actorlogin"}}},
               { '$project': { '_id': 0, 'full_name': "$_id.full_name", 'organization': "$_id.organization",'actorlogin': "$_a1"}}
-              #{ '$sort' : { 'full_name': -1 }}
-              #{ '$limit': 5}
               ]
     mycursor = db.aggregate(pipeline)
-    count = 0
-    batch = 0
-    #https://github.com/nigelsmall/py2neo/issues/361
-    rewrite(("http", "localhost", 0), ("https", os.environ['neoURLProductionURI'], os.environ['neoURLProductionPort'])) 
-    tx = graph.cypher.begin()
-    print MyMoment.MT() + ": #entries to process .... " +  str(len(mycursor['result']))
+    print MyMoment.MT() + ": #new mongodb documents found .... " +  str(len(mycursor['result']))
     sys.stdout.flush()
+    file1 = open(CSVPUBLICDIR + "/repositories.csv", 'w+')
+    file2 = open(CSVPUBLICDIR + "/organizations.csv", 'w+')
+    file4 = open(CSVPUBLICDIR + "/inorganization-relations.csv", 'w+')
+    file3 = open(CSVPUBLICDIR + "/people.csv", 'w+')
+    file5 = open(CSVPUBLICDIR + "/isactor-relations.csv", 'w+')
+    file1.write("id|now\n")
+    file2.write("id|now\n")
+    file3.write("id|now\n")
+    file4.write("a|b\n")
+    file5.write("a|b\n")
+    print MyMoment.MT() +  ": start generating CSV files"
     for record in mycursor["result"]:
-        #print "processing ......",record["full_name"]
-        count = count + 1    
-        statement1 = "MERGE (r:Repository {id:{ID}}) RETURN r"
-        tx.append(statement1,{"ID":record["full_name"]})                   
-        statement2 = "MATCH (r { id: {ID} }) SET r.created_at = {TN} RETURN r"
-        tx.append(statement2,{"ID":record["full_name"],"TN":MyMoment.TNEM()}) 
-        #Create organization
+        #print "processing ... ", record["full_name"]
+        file1.write(record["full_name"] + "|" + str(MyMoment.TNEM()) + "\n")
         if ('organization' in record.keys()):
-            statement3 = "MERGE (o:Organization {id:{ID}}) RETURN o"
-            tx.append(statement3,{"ID":record["organization"]})  
-            statement4 = "MATCH (o { id: {ID} }) SET o.created_at = {TN} RETURN o"
-            tx.append(statement4,{"ID":record["organization"],"TN":MyMoment.TNEM()})
-            #print "creating IN_ORGANIZATION ...."
-            statement5 = "MATCH (o:Organization), (r:Repository) WHERE o.id = {oID} and r.id ={rID} CREATE UNIQUE (r)-[:IN_ORGANIZATION]->(o)"
-            tx.append(statement5,{"rID":record["full_name"],"oID":record["organization"]})
-        #Create actor relation
+            file2.write(record["organization"] + "|" + str(MyMoment.TNEM()) + "\n")
+            file4.write(record["full_name"] + "|" + record["organization"] + "\n")
         for al in record['actorlogin']:
             if (SE(al) == 1):
-                statement6 = "MERGE (p:People {id:{ID}}) RETURN p"
-                tx.append(statement6,{"ID":al})      
-                statement7 = "MATCH (p { id: {ID} }) SET p.created_at = {TN} RETURN p"
-                tx.append(statement7,{"ID":al,"TN":MyMoment.TNEM()})    
-                statement8 = "MATCH (p:People), (r:Repository) WHERE p.id = {pID} and r.id ={rID} CREATE UNIQUE (r)-[:IS_ACTOR]->(p)"
-                tx.append(statement8,{"rID":record["full_name"],"pID":al})
-        #Start processing in small batches
-        if (count > 25):
-            #Keep going!!!
-            batch = batch + 1
-            print MyMoment.MT() + ": start batch ... " + str(batch)
-            sys.stdout.flush()
-            count = 0
-            tx.process()
+                 file3.write(al + "|" + str(MyMoment.TNEM()) + "\n")
+                 file5.write(record["full_name"] + "|" + al + "\n")
+    print MyMoment.MT() +  ": end generating CSV files"
+    file1.close()
+    file2.close()
+    file3.close()
+    file4.close()
+    file5.close()
+    
+def LoadCSV():
+    #Small batch    
+    PC = "USING PERIODIC COMMIT 100\n"
+    r1 = "LOAD CSV WITH HEADERS FROM \"" + CSVPUBLICURL + "/repositories.csv\" AS csvLine FIELDTERMINATOR \"|\""   
+    r2 =  " MERGE (r:Repository {id: csvLine.id}) "
+    r3 =  " SET r.created_at = toInt(csvLine.now)"
+    print MyMoment.MT() +  ": start processing repositories.csv ....."
+    graph.cypher.execute(PC + r1 + r2 + r3)
+    print MyMoment.MT() + ": end processing repositories.csv"    
+    o1 = "LOAD CSV WITH HEADERS FROM \"" + CSVPUBLICURL + "/organizations.csv\" AS csvLine FIELDTERMINATOR \"|\""   
+    o2 =  " MERGE (o:Organization {id: csvLine.id}) "
+    o3 =  " SET o.created_at = toInt(csvLine.now)"
+    print MyMoment.MT() + ": start processing organizations.csv ....."
+    graph.cypher.execute(PC + o1 + o2 + o3)
+    print MyMoment.MT() + ": end processing organizations.csv"    
+    z1 = "LOAD CSV WITH HEADERS FROM \"" + CSVPUBLICURL + "/inorganization-relations.csv\" AS csvLine FIELDTERMINATOR \"|\""   
+    z2 = " MATCH (a:Repository { id: csvLine.a}),(b:Organization {id: csvLine.b}) "
+    z3 = " CREATE UNIQUE (a)-[:IN_ORGANIZATION]->(b)" 
+    print MyMoment.MT() + ": start processing inorganization-relations.csv ....."
+    graph.cypher.execute(PC + z1 + z2 + z3)
+    print MyMoment.MT() + ": end processing inorganization-relations.csv"
+    p1 = "LOAD CSV WITH HEADERS FROM \"" + CSVPUBLICURL + "/people.csv\" AS csvLine FIELDTERMINATOR \"|\""   
+    p2 =  " MERGE (o:People {id: csvLine.id}) "
+    p3 =  " SET o.created_at = toInt(csvLine.now)"
+    print MyMoment.MT() + ": start processing people.csv ....."
+    graph.cypher.execute(PC + p1 + p2 + p3)
+    print MyMoment.MT() + ": end processing people.csv"    
+    z1 = "LOAD CSV WITH HEADERS FROM \"" + CSVPUBLICURL + "/isactor-relations.csv\" AS csvLine FIELDTERMINATOR \"|\""   
+    z2 = " MATCH (a:Repository { id: csvLine.a}),(b:People {id: csvLine.b}) "
+    z3 = " CREATE UNIQUE (a)-[:IS_ACTOR]->(b)" 
+    print MyMoment.MT() + ": start processing isactor-relations.csv ....."
+    graph.cypher.execute(PC + z1 + z2 + z3)
+    print MyMoment.MT() + ": end processing isaction-relations.csv"    
 
-    print MyMoment.MT() + ": start commit transactions ...."
-    sys.stdout.flush()
-    tx.commit()
-    print MyMoment.MT() + ": end commit transactions ....."
-    sys.stdout.flush()
-    print MyMoment.MT() + " end: generating new nodes and building new relations ..."
-    print MyMoment.MT() + ": #Nodes:", Nodes(), " #Relations:",Edges()
+def Cleanup():
+    print MyMoment.MT() + ": #Nodes:", Nodes(), " #Relations:",Edges()    
     print MyMoment.MT() + ": Deleting nodes and relations older than 24 hours ..."
     DayAgo =  MyMoment.TTEM(60*24)
-    d1 = "MATCH (a)-[r]-() where a.created_at <" + str(DayAgo) + " delete a,r"
-    d2 = "MATCH (a) where a.created_at <" + str(DayAgo) + " delete a"
+    d1 = "MATCH (a)-[r]-() where toInt(a.created_at) <" + str(DayAgo) + " delete a,r"
+    d2 = "MATCH (a) where toInt(a.created_at) <" + str(DayAgo) + " delete a"
     graph.cypher.execute(d1)
     graph.cypher.execute(d2)
-    print MyMoment.MT() + ": #Nodes:", Nodes(), " #Relations:",Edges()
- 
-	
-#Create Nodes & Relations
-CNR()
+    print MyMoment.MT() + ": #Nodes:", Nodes(), " #Relations:",Edges()    
+
+#Create constraints & index
+CC()        
+#Generate CSV files with nodes & relations
+GenerateCSV()
+print MyMoment.MT() + ": #Nodes:", Nodes(), " #Relations:",Edges() 
+print MyMoment.MT() + ": start inserting ....."
+#Load CSV files
+LoadCSV()
+print MyMoment.MT() + ": end inserting"
+
+#Delete old nodes and relationss
+Cleanup()
+
+
