5575584bcf1cf60a7b855675f2deae2f7bcab852
harishvc <harishvc@gmail.com>
2015-03-16 22:03:18 -0700

    add try catch

--- bin/GitHubAPIRateLimit.py
+++ bin/GitHubAPIRateLimit.py
@@ -6,10 +6,14 @@ sys.path.append('../')
 import MyMoment
 
 #Get rate limit for application
-
 client_id = os.environ['github_client_id']
 client_secret = os.environ['github_client_secret']
 url ="https://api.github.com/rate_limit?client_id=" + client_id + "&client_secret=" + client_secret
 
-response = requests.get(url)
-print "Limit: " +  response.headers["X-RateLimit-Limit"] + " Remaining: " + response.headers["X-RateLimit-Remaining"] + " Reset: " + MyMoment.HTM(int(response.headers["X-RateLimit-Reset"]),"from now")
+try:
+    response = requests.get(url)
+    print "Limit: " +  response.headers["X-RateLimit-Limit"] + " Remaining: " + response.headers["X-RateLimit-Remaining"] + " Reset: " + MyMoment.HTM(int(response.headers["X-RateLimit-Reset"]),"from now")
+except:
+    print "####### Unexpected error #######"
+    print sys.exc_info()[0]
+    raise
