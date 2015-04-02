67446649977b979a76ac4d7b09a1090ca72a1d40
harishvc <harishvc@gmail.com>
2015-04-02 00:13:50 -0700

    Request to get repository languages & horizontal bar charts are generated after page is loaded (harishvc/githubanalytics#51), dynamically bind event handlers after new elements are created (harishvc/githubanalytics#28)

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+
+    var DrawChart = function() {         
+    //Create horizontal bar chart using JQuery
+    $('.chart').horizBarChart({selector: '.bar',speed: 3000});
+    };
+
+    var cleanup = function() {
+        $('#searchbox #intro').empty();
+        $('#searchbox #intro2').empty();
+        $('#askgithub').empty();
+        $('#result').empty();
+        $('#progress').show();
+    }
+    
     //Handle Sumbit
     $('#askgithub').submit(function() {
         $('#searchbox #intro').empty();
@@ -13,24 +27,14 @@ $(document).ready(function() {
     }
     
     //Bind to hyperlinks 
-    $('a.repositoryinfo').bind('click', function() {
-        $('#searchbox #intro').empty();
-        $('#searchbox #intro2').empty();
-         $('#askgithub').empty();
-        $('#result').empty();
-        $('#progress').show();
-    });
-  
-  
-    $("a").on("click", "repositoryinfo", function(event){
-        alert($(this).text());
-    });
-  
+    $('a.repositoryinfo').bind('click', cleanup);
+   
+   
     //Find similare repositories on demand
     $('a#findsimilarrepos').bind('click', function() {
         $("#wrapperfindsimilarrepos").empty();
         $("#wrapperfindsimilarrepos").html("finding similar repositories just for you <i class=\"fa fa-spinner fa-spin fa-1x\"></i>");
-        $.ajax({
+        var d2 = $.ajax({
             url : '/_findsimilarrepositories',
             dataType : 'json',
             timeout : (60000),
@@ -46,11 +50,29 @@ $(document).ready(function() {
                 $("#wrapperfindsimilarrepos").empty();
             }
         });
+        $.when( d2 ).done(function(){
+            $('a.repositoryinfo').bind('click',cleanup);
+        });
         return false;
     });
-         
-    //Create horizontal bar chart using JQuery
-    $('.chart').horizBarChart({selector: '.bar',speed: 3000});
-      
+    
+    //Automatically display Horizontal Bar Charts
+    if ($('#listlanguages').length > 0) {
+           //Step1: Wait for the page to load! 
+           $(window).bind("load", function() {
+           $("#listlanguages").empty();$("#listlanguages").html("Lising languages <i class=\"fa fa-spinner fa-spin fa-1x\"></i>");
+           //Step2: Function call using AJAX
+           var d1 = $.ajax({url:'/_listlanguages',dataType:'json', timeout: (60000) ,data: {a: $('input[name="reponame"]').val()},
+                  success: function(data) {$("#listlanguages").empty();$("#listlanguages").html(data.languages);},
+                  error: function(objAJAXRequest, strError){$("#listlanguages").html("<span class=\"text-danger\">Query taking too long: Please try again latter</span>")} 
+                  });
+           //Step3: Wait for AJAX to return, attach event handler to the new dynamic element
+           $.when( d1 ).done(function(){
+            $('#listlanguages').on('click',DrawChart);
+            DrawChart(); //trigger a click event
+            });
+        });
+     }
+       
 });
 
