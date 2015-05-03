5f1448d3b60dab454fb40fe6468e7330f1d5f387
harishvc <harishvc@gmail.com>
2015-05-02 22:45:23 -0700

    display intresting topics based on contributor comments using prismatic API - harishvc/githubanalytics#63

--- GetPrismatic.py
+++ GetPrismatic.py
@@ -1,11 +1,23 @@
 #Source: http://neo4j.com/blog/neo4j-building-topic-graph-prismatic-interest-graph-api/
 #Reference: https://github.com/Prismatic/interest-graph#topic-tagging
 
+from pymongo import MongoClient
 import time
 import requests
 import json
 import os
- 
+import re
+
+#Configure for production or development based on environment variables
+if (os.environ['deployEnv'] == "production"):
+    MONGO_URL = os.environ['connectURLRead']
+    connection = MongoClient(MONGO_URL)
+    db = connection.githublive.pushevent
+else: 
+    MONGO_URL = os.environ['connectURLReaddev']
+    connection = MongoClient(MONGO_URL)
+    db = connection.githubdev.pushevent
+        
 def RateLimited(maxPerSecond):
     minInterval = 1.0 / float(maxPerSecond)
     def decorate(func):
@@ -27,3 +39,40 @@ def topics(body):
                 'body': body,
                 'api-token': os.environ['prismatickey']} 
     return (requests.post("http://interest-graph.getprismatic.com/text/topic", data=payload))
+
+def GetInterestTopics(type,value):
+    pipeline = [
+                    {'$match':{'$and':[{'type':{'$in':["PushEvent"]}},{type:value}]}}, 
+                    {'$group':{'_id':{type:value},'comments':{"$addToSet":"$comment"}}} 
+                ]
+    mycursor = db.aggregate(pipeline)
+    #print mycursor
+    P1 = ""
+    P2 = ""
+    rt = []  #returned topics
+    output = "<p class=\"tpadding text-success\">" + "Trending topic(s) in organization " + value + "</p>"
+    for row in mycursor["result"]: 
+        P1 = ''.join(row['comments']) 
+        #clean up: remove spaces
+        #TODO: remove special characters, html, +++ , ====
+        P2 = re.sub( '\s+', ' ', P1).strip()
+    #Condition 1: Call Prismatic API when there is more than 1000 characters in comments    
+    if len(P2) > 1000:
+        P3 = ""
+        #Condition 2: Send only 2k characters
+        P3 = topics(P2[0:20000]).json()
+        #Condition 3: Handle no topic return 
+        if len(P3['topics']) > 0:
+            for topic in P3['topics']:
+                rt.append(topic['topic'])
+            if len(rt) > 1:
+                output = "<p class=\"tpadding text-success\">" + "Trending topics in organization " + value + "</p>"
+            else:
+                output = "<p class=\"tpadding text-success\">" + "Trending topic in organization " + value + "</p>"
+            output += ', '.join(rt)     
+        else:
+            output = "Sorry, found no trending topics based on contributor comment(s)"
+    else:
+        output = "No trending topics found based on contributor comment(s)"
+    return output
+    
