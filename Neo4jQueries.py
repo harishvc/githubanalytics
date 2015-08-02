b8b256d33c8f898b82df310991b6064cd7faa5e8
harishvc <harishvc@gmail.com>
2015-08-02 07:15:25 -0700

    Change Neo4j env variables

--- Neo4jQueries.py
+++ Neo4jQueries.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 def FindSimilarRepositories(InputrepoK):
 	#Sanitize input
 	Inputrepo = bleach.clean(InputrepoK).strip()
-	graph = Graph(os.environ['neoURLProduction'])
+	host  = os.environ['LOCALNEO4JIPPORT']
+	login = os.environ['LOCALNEO4JLOGIN']
+	password = os.environ['LOCALNEO4JPASSWORD']
+	authenticate(host,login,password)
+	graph = Graph(os.environ['neoURLlocal'])
 	output = ""
 	path1 = "<a href=\"/?q=repository "
 	path2 = "&amp;action=Search\"  class=\"repositoryinfo\">"
@@ -17,7 +21,7 @@ def FindSimilarRepositories(InputrepoK):
 	query1="MATCH (a {id:\"" + Inputrepo + "\"})"
 	query2="-[r1:IS_ACTOR|IN_ORGANIZATION]->(match)<-[r2:IS_ACTOR|IN_ORGANIZATION]-(b) "
 	query3="with b, collect (distinct match.id) as connections, collect (distinct type(r1)) as rel1 "
-	query4="where length(connections) >= 3 return b.id,length(connections) as count,length(rel1) as rel "
+	query4="where length(connections) >= 1 return b.id,length(connections) as count,length(rel1) as rel "
 	query5="order by length(connections)  desc limit 5" 
 	
 	query = query1 + query2 + query3 + query4 + query5 
