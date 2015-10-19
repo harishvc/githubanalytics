34fc72723d4694a67f16381255d0173b739cbda5
harishvc <harishvc@gmail.com>
2015-10-19 00:01:50 -0700

    Redis worker queue

---
+++ worker.py
@@ -0,0 +1,16 @@
+import os
+import redis
+from rq import Worker, Queue, Connection
+
+from Neo4jQueries import FindSimilarRepositories   
+
+listen = ['high', 'default', 'low']
+
+redis_url = os.getenv('REDISTOGO_URL', 'redis://127.0.0.1:6379')
+
+conn = redis.from_url(redis_url)
+
+if __name__ == '__main__':
+    with Connection(conn):
+        worker = Worker(map(Queue, listen))
+        worker.work()
