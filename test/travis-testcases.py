a7af395c35699a201fb8267a7da2f94c6badda61
harishvc <harishvc@gmail.com>
2015-10-19 01:16:20 -0700

    add SauceClient :bulb:

--- test/travis-testcases.py
+++ test/travis-testcases.py
@@ -3,6 +3,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from sauceclient import SauceClient
 import os
 
 #Get Environment variables
