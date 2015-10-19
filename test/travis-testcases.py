1818e8997f92ff4c80b242b67e11dcf45c8b98c6
harishvc <harishvc@gmail.com>
2015-10-18 20:03:34 -0700

    integration with Sauce :boom:

--- test/travis-testcases.py
+++ test/travis-testcases.py
@@ -1,14 +1,44 @@
-#http://selenium-python.readthedocs.org/en/latest/getting-started.html
+#Source:https://docs.saucelabs.com/tutorials/python/#getting-started-with-python
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import os
 
-driver = webdriver.Firefox()
+#Get Environment variables
+if os.environ.get('TRAVIS_COMMIT') is not None:
+    version = os.environ.get('TRAVIS_COMMIT')
+else:
+    version = ""
+if os.environ.get('TRAVIS_BUILD_NUMBER') is not None:
+    build = os.environ.get('TRAVIS_BUILD_NUMBER')
+else:
+    build = ""
+    
+desired_cap = {
+    'platform': "Mac OS X 10.9",
+    'browserName': "chrome",
+    'version': version,
+    'build': build,
+}
+
+#Sauce Labs
+SauceLogin = os.environ['SauceLogin']
+SauceAccessKey = os.environ['SauceAccessKey']
+
+driver = webdriver.Remote(
+   command_executor="http://%s:%s@ondemand.saucelabs.com:80/wd/hub" % (SauceLogin, SauceAccessKey),
+   desired_capabilities=desired_cap)
+
+# This is your test logic. You can add multiple tests here.
+driver.implicitly_wait(10)
 driver.get("http://localhost:5000")
 assert "Ask GitHub" in driver.title
 elem = driver.find_element_by_name("q")
 elem.send_keys("trending now")
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
-driver.close()
+
+# This is where you tell Sauce Labs to stop running tests on your behalf.  
+# It's important so that you aren't billed after your test finishes.
+driver.quit()
